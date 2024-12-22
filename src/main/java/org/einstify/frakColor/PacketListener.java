4047cd74a7393935c69950505d11fb84c9685fa6
EinsTify <einstify@gmail.com>
2024-12-22 15:51:00 +0100

    kleine anpassung unnötige abfrage entfernt, Logger angepasst

--- src/main/java/org/einstify/frakColor/PacketListener.java
+++ src/main/java/org/einstify/frakColor/PacketListener.java
@@ -19,27 +19,25 @@ public class PacketListener implements com.github.retrooper.packetevents.event.P
     public void onPacketSend(PacketSendEvent event) {
         if (event.getPlayer() instanceof Player && event.getPacketType() == PacketType.Play.Server.PLAYER_INFO) {
             //String receiverFrak = playerManager.getPlayerData(event.getPlayer).getFaction();
-            if (event.getPacketType() != null) {
-                try {
-                    WrapperPlayServerPlayerInfo playerInfo = new WrapperPlayServerPlayerInfo(event);
-                    List<WrapperPlayServerPlayerInfo.PlayerData> dataList = new ArrayList<>();
-                    if (playerInfo.getPlayerDataList() != null && !playerInfo.getPlayerDataList().isEmpty()) {
-                        for (WrapperPlayServerPlayerInfo.PlayerData data : playerInfo.getPlayerDataList()) {
-                            if (data.getDisplayName() != null) {
-                                //if(playerManager.getPlayerData(Bukkit.getPlayer(String.valueOf(data.getDisplayName()))).getFaction().equals(receiverFrak)) {
-                                    data.setDisplayName(Component.text(data.getDisplayName().toString()).color(NamedTextColor.BLUE));
-                                    dataList.add(data);
-                                //}
-                            }
+            try {
+                WrapperPlayServerPlayerInfo playerInfo = new WrapperPlayServerPlayerInfo(event);
+                List<WrapperPlayServerPlayerInfo.PlayerData> dataList = new ArrayList<>();
+                if (playerInfo.getPlayerDataList() != null && !playerInfo.getPlayerDataList().isEmpty()) {
+                    for (WrapperPlayServerPlayerInfo.PlayerData data : playerInfo.getPlayerDataList()) {
+                        if (data.getDisplayName() != null) {
+                            //if(playerManager.getPlayerData(Bukkit.getPlayer(String.valueOf(data.getDisplayName()))).getFaction().equals(receiverFrak)) {
+                                data.setDisplayName(Component.text(data.getDisplayName().toString()).color(NamedTextColor.BLUE));
+                                dataList.add(data);
+                            //}
                         }
                     }
-                    playerInfo.setPlayerDataList(dataList);
-                    event.getUser().sendPacket(playerInfo);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    StringWriter stack = new StringWriter();
-                    e.printStackTrace(new PrintWriter(stack));
-                    Bukkit.getLogger().info("Fehler beim Nametag laden: " + e.getMessage() + stack.toString());
                 }
+                playerInfo.setPlayerDataList(dataList);
+                event.getUser().sendPacket(playerInfo);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                StringWriter stack = new StringWriter();
+                e.printStackTrace(new PrintWriter(stack));
+                Bukkit.getLogger().info("Fehler beim Nametag laden: " + e.getMessage() + " " + stack.toString());
             }
         }
     }
